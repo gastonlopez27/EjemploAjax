304e8ab357ef7071616ad8c1455919f09bd805a5
gastonlopez27 <adpgastonlopez@gmail.com>
2016-09-26 20:59:43 -0300

    primer comit

--- js/funcionesAjax.js
+++ js/funcionesAjax.js
@@ -1,6 +1,6 @@
 
 function MostrarError()
-{
+{/*
 	var funcionAjax=$.ajax({url:"nexoNoExiste.php",type:"post",data:{queHacer:"MostrarTexto"}});
 	funcionAjax.done(function(retorno){
 		$("#principal").html(retorno);
@@ -13,9 +13,25 @@ function MostrarError()
 	funcionAjax.always(function(retorno){
 		//alert("siempre "+retorno.statusText);
 	});
+*/
+//en .ajax() adentro de los parentesis va json entre {}
+$.ajax({url:"nexoNoExiste.php"}).then(function(respuesta){
+alert("Primero" + respuesta);
+console.info("Primero", respuesta)
+},function(respuesta){
+alert("Segundo" + respuesta);
+console.info("Segundo", respuesta)
+}); 
+//esta funcion devuelve retornos o callbacks. Este retorno nos puede dar error o correcto. 
+//el crrecto nos trae el codigo. 
+//then recibe 2 funciones completas por parametro
+//ajax sirve para comunicarme con la bbdd o con una API
+//dentro del then recibe 2 tipos de callbacks, correcta o error
+
 }
 function MostrarSinParametros()
 {
+	/*
 	var funcionAjax=$.ajax({url:"nexoTexto.php"});
 
 	funcionAjax.done(function(retorno){
@@ -30,6 +46,17 @@ function MostrarSinParametros()
 		//alert("siempre "+retorno.statusText);
 
 	});
+*/
+
+$.ajax({url:"nexoTexto.php"}).then(
+	function(respuesta){
+		$("#principal").html(respuesta);
+	}
+	,
+	function(error){
+		$("#informe").html(error.responseText);
+	}
+	);
 }
 
 function Mostrar(queMostrar)
